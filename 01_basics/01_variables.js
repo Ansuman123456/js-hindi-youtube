e94e3d7d4ab74ceaa28a9d7d58f400e3228c0f27
Ansuman Rout <routanshuman198@gmail.com>
2026-09-22 14:18:28 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 8520532
+let accountEmail = "ansuman@google.com"
+var accountPassword ="12345"
+accountCity = "Kendrapada"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail="mahendra@google.ccom"
+accountPassword="789456"
+accountCity="odisha"
+
+console.log(accountId);
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
